541b9c3df2f90de57e9694e359261991aa905d88
Henry <152362527+Henry020@users.noreply.github.com>
2026-07-15 05:55:44 -0300

    Add files via upload

---
+++ Desafio 5.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um numero: '))
+a = n - 1
+b = n + 1
+print(' seu antecessor é {}, e seu sucessor é {}'.format(a,b))
+#print('seu antecessor é {}, e seu sucessor é {}'.format((n-1),(n+1))
